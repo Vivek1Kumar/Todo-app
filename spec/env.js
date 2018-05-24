14e927f9fffc46e7adbb124c00ad80db8110c1df
Vivek1kumar <vivekyadav264@gmail.com>
2018-05-24 03:01:19 -0700

    first commit

--- spec/env.js
+++ spec/env.js
@@ -1,14 +1,14 @@
-// const fs = require('fs');
+const fs = require('fs');
 
-// describe("Environment", () =>  {
+describe("Environment", () =>  {
 
-//   it('os is linux-based', () => {
-//     expect(process.platform).toBe('linux');
-//   });
+  it('os is linux-based', () => {
+    expect(process.platform).toBe('linux');
+  });
 
-//   it('has locally installed node modules', () => {
-//     var node_module_dir_path = './node_modules';
-//     expect(fs.lstatSync(node_module_dir_path).isDirectory()).toBeTruthy();
-//   });
+  it('has locally installed node modules', () => {
+    var node_module_dir_path = './node_modules';
+    expect(fs.lstatSync(node_module_dir_path).isDirectory()).toBeTruthy();
+  });
 
-// });
+});
